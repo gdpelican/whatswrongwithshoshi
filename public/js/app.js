c623da1d7e64dfe395e787a49eea1df72b55f126
James Kiesel <james.kiesel@gmail.com>
2014-12-07 23:19:06 +1300

    Add a form

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,51 @@
 var questionFaders = $('.question-fade-in');
 var answerFaders = $('.answer-fade-in');
 
-
 var fadeIn = function(faders, index, timeout) {
   $(faders[index]).addClass('faded-in');
   if(index < faders.length) {
     setTimeout(function() { fadeIn(faders, index+1, timeout); }, timeout);
   }
 };
+setTimeout(function() { fadeIn(questionFaders, 0, 1000); }, 1000);
 
-setTimeout(function() { fadeIn(questionFaders, 0, 1500); }, 1000);
+var whatsWrong = function(whatsWrong) {
+  whats_wrong = $('.whats-wrong')
+  question = $('.question');
+  overlay = $('.overlay');
+  shoshiInput = $('.shoshi-input');
 
-$('button.btn-whats-wrong').on('click', function() {
-  $.get('/whatswrong', function(data) {
-    whats_wrong = $('.whats-wrong')
-    question = $('.question');
-
+  shoshiInput.val('');
+  overlay.fadeOut(function() {
     if(question.is(':visible')) {
-      whats_wrong.html(data)
+      whats_wrong.html(whatsWrong)
       question.fadeOut(function() {
-        fadeIn(answerFaders, 0, 750);
+        fadeIn(answerFaders, 0, 600);
       });
     }
     else {
       whats_wrong.removeClass('faded-in');
       setTimeout(function() {
-        whats_wrong.html(data)
+        whats_wrong.html(whatsWrong)
         whats_wrong.addClass('faded-in');
       }, 750);
     }
   });
+}
+
+$('button.btn-whats-wrong').on('click', function() {
+  $.get('/whatswrong', whatsWrong);
 })
+
+$('button.thats-whats-wrong').on('click', function(e) {
+  e.preventDefault();
+  $.post('/thatswhatswrong', {description: $('.shoshi-input').val()}, whatsWrong);
+});
+
+$('button.i-have-one').on('click', function() {
+  $('.overlay').fadeIn();
+});
+
+$('button.i-dont-have-one').on('click', function() {
+  $('.overlay').fadeOut();
+});
